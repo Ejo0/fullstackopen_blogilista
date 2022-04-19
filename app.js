70249dbc193bf6930927e675ea302a833b32c3c9
Erno Jokela <38183216+Ejo0@users.noreply.github.com>
2022-04-19 08:46:38 +0300

    4.18: token based authentication

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const app = express()
 const cors = require('cors')
 const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 const mongoose = require('mongoose')
 const middleWare = require('./utils/middleware')
 
@@ -14,6 +15,7 @@ app.use(cors())
 app.use(express.json())
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 app.use(middleWare.errorHandler)
 
 module.exports = app
