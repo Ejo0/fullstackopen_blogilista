a94a2831ff70581d3f9452afee42aec6d3e60e0d
Erno Jokela <38183216+Ejo0@users.noreply.github.com>
2022-04-19 09:04:00 +0300

    4.19: check token when creating new blog

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,6 +1,15 @@
+const jwt = require('jsonwebtoken')
 const blogsRouter = require('express').Router()
 const Blog = require("../models/blog")
 const User = require('../models/user')
+
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    return authorization.substring(7)
+  }
+  return null
+}
 
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog
@@ -11,21 +20,25 @@ blogsRouter.get('/', async (request, response) => {
 
 blogsRouter.post('/', async (request, response) => {
   const body = request.body
-
-  const userFoo = await User.findById('625d7d772cec3b828e6e6f30')
+  const token = getTokenFrom(request)
+  const decodedToken = jwt.verify(token, process.env.SECRET_KEY)
+  if (!token || !decodedToken.id) {
+    return response.status(401).json({ error: 'token missing or invalid' })
+  }
+  const user = await User.findById(decodedToken.id)
 
   const blog = new Blog({
     title: body.title,
     author: body.author,
     url: body.url,
     likes: body.likes || 0,
-    user: userFoo._id
+    user: user._id
   })
 
   const savedBlog = await blog.save()
 
-  userFoo.blogs = userFoo.blogs.concat(savedBlog._id)
-  await userFoo.save()
+  user.blogs = user.blogs.concat(savedBlog._id)
+  await user.save()
 
   response.status(201).json(savedBlog)
 })
--- utils/middleware.js
+++ utils/middleware.js
@@ -2,11 +2,15 @@ const errorHandler = (error, request, response, next) => {
     if (error.name === 'ValidationError') {
         return response.status(400).json({ error: error.message })
     }
-    next(error)
 
     if (error.name === 'CastError') {
         return response.status(400).send({ error: 'malformatted id' })
     }
+
+    if (error.name === 'JsonWebTokenError') {
+        return response.status(401).json({ error: 'invalid token'})
+    }
+    next(error)
 }
 
 module.exports = { errorHandler }
